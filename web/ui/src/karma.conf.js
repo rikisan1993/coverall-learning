8e71e524c994c69aa63caccc45b141948533ee78
rikisan1993 <kosiriki@gmail.com>
2019-04-01 14:49:44 +0700

    updated karma and ci

--- web/ui/src/karma.conf.js
+++ web/ui/src/karma.conf.js
@@ -1,6 +1,9 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
 
+const process = require('process');
+process.env.CHROME_BIN = require('puppeteer').executablePath();
+
 module.exports = function (config) {
   config.set({
     basePath: '',
@@ -25,7 +28,13 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
+    browsers: ['Chrome_no_sandbox'],
+    customLaunchers:{
+      Chrome_no_sandbox:{
+        base: 'Chrome_no_sandbox',
+        flags: ['--no-sandbox']
+      }
+    },
     singleRun: false,
     restartOnFileChange: true
   });
